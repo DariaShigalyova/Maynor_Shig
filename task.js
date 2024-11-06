451a9692eefe7ba9942795fd694a6c5db08f1494
DariaShigalyova <shigalyovada2004@gmail.com>
2024-11-06 14:30:51 +0300

    task.js

--- task.js
+++ task.js
@@ -6,10 +6,10 @@ document.getElementById("checkButton").addEventListener("click", function() {
     const c = parseFloat(document.getElementById("c").value);
     const d = parseFloat(document.getElementById("d").value);
 
-    // Проверяем, может ли внутренний прямоугольник уместиться внутри внешнего
+  
     const canFit = (a <= c && b <= d) || (a <= d && b <= c);
 
-    // Выводим результат
+
     const resultElement = document.getElementById("result");
     if (canFit) {
         resultElement.textContent = "Внутренний прямоугольник может уместиться внутри внешнего.";
